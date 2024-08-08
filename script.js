a955eadf39823614db5543a144349aebb10372b6
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 15:05:08 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Script running...");
+// Task 1: CSH Mode
+// Select the CSH mode button and console log to confirm you selected successfully
+let button =
+
+// Select the body and console log to confirm you selected successfully
+
+
+// Add an event listener such that when you click the CSH mode button, the background color of body becomes green
+
+
+
+
+
+// Task 2: All off button
+// Select the all off button and console log to confirm you selected successfully
+
+
+// Select all the lightbulb p tags and console log the length to confirm you selected successfully
+
+
+// Add an event listener such that when the all off button is clicked, the background color of all the lightbulbs becomes black
+
+
+
+
+
+// Task 3: On and off feature
+// Add an event listener to the FIRST lightbulb such that when you click it, the background color of that p tag changes to white
+
+
+
+
+
+
+// Extra credit: try to use a for loop to make every lightbulb's background color to change when you click it
+
+
+
+
+
+
